bc096a17a73fb51b2ebd7037ccecf123783581fa
Asadullah <asadullahishfaq8@gmail.com>
2022-07-13 13:41:42 +0500

    updated

--- dogo/src/App.js
+++ dogo/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "container">
+      <nav class="navbar bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href='index.html'>Dogo</a>
+        </div>
+      </nav>
     </div>
+    
   );
 }
 
