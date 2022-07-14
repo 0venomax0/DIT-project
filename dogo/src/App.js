4aa72737163b6b98a32c7b7e1849407a7cc56401
Asadullah <asadullahishfaq8@gmail.com>
2022-07-14 17:32:40 +0500

    ok

--- dogo/src/App.js
+++ dogo/src/App.js
@@ -2,14 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div class = "container">
-      <nav class="navbar bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href='index.html'>Dogo</a>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto" >
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Training Program</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
-    
+
   );
 }
 
